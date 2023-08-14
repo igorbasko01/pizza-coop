4519af60e81edf107539675502141d224c5b3b47
igorbasko01 <igorbasko@gmail.com>
2023-08-14 07:39:45 +0300

    emit empty loaded state

--- lib/bloc/stock_role_bloc.dart
+++ lib/bloc/stock_role_bloc.dart
@@ -1,7 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_coop/bloc/stock_role_event.dart';
 import 'package:pizza_coop/bloc/stock_role_state.dart';
+import 'package:pizza_coop/domain/stock_role.dart';
 
 class StockRoleBloc extends Bloc<StockRoleEvent, StockRoleState> {
-  StockRoleBloc() : super(InitialStockRoleState());
+  final StockRole stockRole;
+
+  StockRoleBloc({required this.stockRole}) : super(InitialStockRoleState()) {
+    on<LoadIngredientsStockRoleEvent>(_onLoadIngredientsStockRoleEvent);
+  }
+
+  void _onLoadIngredientsStockRoleEvent(
+      LoadIngredientsStockRoleEvent event, Emitter<StockRoleState> emit) {
+    emit(LoadingIngredientsStockRoleState());
+    emit(LoadedIngredientsStockRoleState(stockRole.listStock()));
+  }
 }
