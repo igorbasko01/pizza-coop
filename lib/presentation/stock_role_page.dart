36d637748e4aca1ab5777a0a254f382777283b87
igorbasko01 <igorbasko@gmail.com>
2023-09-06 19:55:39 +0300

    connect the new page

--- lib/presentation/stock_role_page.dart
+++ lib/presentation/stock_role_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_coop/bloc/ingredients_catalog_bloc.dart';
 import 'package:pizza_coop/bloc/ingredients_stock_bloc.dart';
 import 'package:pizza_coop/domain/ingredients/ingredient.dart';
 import 'package:pizza_coop/domain/ingredients/ingredients_catalog.dart';
 import 'package:pizza_coop/domain/ingredients/ingredients_stock.dart';
 import 'package:pizza_coop/domain/stock_role.dart';
 import 'package:pizza_coop/domain/wallet.dart';
+import 'package:pizza_coop/presentation/ingredients_catalog_page_view.dart';
 import 'package:pizza_coop/presentation/ingredients_stock_page_view.dart';
 
 class StockRolePage extends StatelessWidget {
@@ -21,9 +23,14 @@ class StockRolePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => IngredientsStockBloc(
-          stockRole: stockRole),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<IngredientsCatalogBloc>(
+          create: (context) => IngredientsCatalogBloc(stockRole: stockRole),
+        ),
+        BlocProvider<IngredientsStockBloc>(
+          create: (context) => IngredientsStockBloc(stockRole: stockRole),
+        ),],
       child: const StockRolePageView(),
     );
   }
@@ -52,9 +59,11 @@ class StockRolePageView extends StatelessWidget {
           ElevatedButton(
             child: const Text('Catalog'),
             onPressed: () {
-              const snackBar =
-                  SnackBar(content: Text('Catalog under construction'));
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              Navigator.push(context, MaterialPageRoute(builder: (_) {
+                return BlocProvider.value(
+                  value: BlocProvider.of<IngredientsCatalogBloc>(context),
+                  child: const IngredientsCatalogPageView());
+              }));
             },
           ),
         ],
