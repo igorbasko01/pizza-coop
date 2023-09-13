bccfa3a848be514d07449017cdd5d189b6ff24e7
igorbasko01 <igorbasko@gmail.com>
2023-09-13 20:26:30 +0300

    add order class (#69)

---
+++ test/domain/order_test.dart
@@ -0,0 +1,19 @@
+import 'package:pizza_coop/domain/customer.dart';
+import 'package:pizza_coop/domain/ingredients/ingredient.dart';
+import 'package:pizza_coop/domain/order.dart';
+import 'package:pizza_coop/domain/recipe.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Order initialized with Recipe and a Customer', () {
+    var recipe = Recipe('Pizza', [
+      StockIngredient('Flour', 1.5),
+      StockIngredient('Tomato Sauce', 0.5),
+      StockIngredient('Cheese', 2.0),
+    ]);
+    var customer = Customer('John Doe');
+    var order = Order(recipe, customer);
+    expect(order.recipe, recipe);
+    expect(order.customer, customer);
+  });
+}
