2f8bad5fbc722c893cf37e0ec5210239ae4b6a8c
igorbasko01 <igorbasko@gmail.com>
2023-09-22 18:32:47 +0300

    add ingredients stock to cook role

--- lib/domain/cook_role.dart
+++ lib/domain/cook_role.dart
@@ -1,7 +1,9 @@
+import 'package:pizza_coop/domain/ingredients/ingredients_stock.dart';
 import 'package:pizza_coop/domain/oven.dart';
 
 class CookRole {
   final Oven oven;
+  final IngredientsStock ingredientsStock;
 
-  CookRole(this.oven);
+  CookRole(this.oven, this.ingredientsStock);
 }
--- test/domain/cook_role_test.dart
+++ test/domain/cook_role_test.dart
@@ -1,11 +1,20 @@
 import 'package:pizza_coop/domain/cook_role.dart';
+import 'package:pizza_coop/domain/ingredients/ingredients_stock.dart';
 import 'package:pizza_coop/domain/oven.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Cook Role Initializes with an Oven', () {
     var oven = Oven();
-    var role = CookRole(oven);
+    var ingredientsStock = IngredientsStock();
+    var role = CookRole(oven, ingredientsStock);
     expect(role.oven, isNotNull);
+  });
+
+  test('Cook Role Initializes with an IngredientsStock', () {
+    var oven = Oven();
+    var ingredientsStock = IngredientsStock();
+    var role = CookRole(oven, ingredientsStock);
+    expect(role.ingredientsStock, isNotNull);
   });
 }
