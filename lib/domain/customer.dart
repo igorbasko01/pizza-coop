011a807ce9022352900045b81ace866cf53f4726
igorbasko01 <igorbasko@gmail.com>
2023-09-26 22:31:55 +0300

    customer accepts a prepared ingredient (#97)

--- lib/domain/customer.dart
+++ lib/domain/customer.dart
@@ -1,14 +1,30 @@
+import 'package:pizza_coop/domain/ingredients/ingredient.dart';
 import 'package:pizza_coop/domain/menu.dart';
 import 'package:pizza_coop/domain/recipe.dart';
 
 class Customer {
   final int id;
   String name;
-  bool isSatisfied = false;
+  bool _isSatisfied = false;
+  Recipe? _selectedRecipe;
 
   Customer(this.id, this.name);
 
   Recipe selectRecipe(Menu menu) {
-    return menu.recipes.first;
+    _selectedRecipe ??= menu.recipes.first;
+    return _selectedRecipe!;
   }
+
+  bool get isSatisfied => _isSatisfied;
+
+  void accept(StockIngredient stockIngredient) {
+    if (_selectedRecipe == null) throw NoRecipeSelectedException('No recipe selected');
+    if (stockIngredient.name == _selectedRecipe?.name) _isSatisfied = true;
+  }
+}
+
+class NoRecipeSelectedException implements Exception {
+  final String message;
+
+  NoRecipeSelectedException(this.message);
 }
--- test/domain/customer_test.dart
+++ test/domain/customer_test.dart
@@ -35,4 +35,31 @@ void main() {
     var customer = Customer(1, 'John');
     expect(customer.isSatisfied, false);
   });
+
+  test('Customer is satisfied when provided with a StockIngredient that matches the recipe', () {
+    var customer = Customer(1, 'John');
+    var menu = Menu(recipes: [
+      Recipe('Pizza', [
+        StockIngredient('Flour', 1.5),
+        StockIngredient('Tomato Sauce', 0.5),
+        StockIngredient('Cheese', 2.0),
+      ]),
+      Recipe('Calzone', [
+        StockIngredient('Flour', 1.5),
+        StockIngredient('Tomato Sauce', 0.5),
+        StockIngredient('Cheese', 2.0),
+        StockIngredient('Ham', 1.0),
+      ]),
+    ]);
+    var stockIngredient = StockIngredient('Pizza', 1);
+    customer.selectRecipe(menu);
+    customer.accept(stockIngredient);
+    expect(customer.isSatisfied, true);
+  });
+
+  test('Customer cannot accept a StockIngredient before a recipe is selected', () {
+    var customer = Customer(1, 'John');
+    var stockIngredient = StockIngredient('Pizza', 1);
+    expect(() => customer.accept(stockIngredient), throwsA(isA<NoRecipeSelectedException>()));
+  });
 }
